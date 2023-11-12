960da8deb4d16c10900bbdc9d7157be00692bd16
Giselle Torres <giselita6313@gmail.com>
2023-11-12 17:47:48 -0500

    Login 1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { logincomponent } from './login/login.component';
-const routes: Routes = [{path: '', component: logincomponent}];
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [{path: '', component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
